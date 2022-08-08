3df1d8ba977b7110b8678fd21c0906ef7c21f083
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-08-08 09:49:54 +0300

    adding getter functions

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -115,5 +115,12 @@ contract MultiSigWallet {
     receive() external payable {
         emit Deposit(msg.sender, msg.value, address(this).balance);
     }
+
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
+    function getTransactionCount() public view returns (uint) {
+        return transactions.length;
+    }
 }
-//["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"]
